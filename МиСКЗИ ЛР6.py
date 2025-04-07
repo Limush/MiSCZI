ce2fcef674edc1935ed66be26180fcb39bb38314
Limush <dzaikin999@gmail.com>
2025-04-07 15:20:18 +0300

    version 2.0

--- МиСКЗИ ЛР6.py
+++ МиСКЗИ ЛР6.py
@@ -4,9 +4,16 @@ import math
 
 
 alfavit = ' АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'
-text = input("Введите текст ->")
-mass = [alfavit.find(i) for i in text]
 
+p = sympy.randprime(2 ** 1021, 2 ** 1022)
+q = random.randint(2, p)
+while math.gcd(q, p) != 1:
+    q = random.randint(2, p)
+x = random.randint(2, p - 2)
+y = pow(q, x, p)
+print(f"p = {p}\nq = {q}\nx = {x}\ny = {y}\n"
+      f"Открытый ключ:\n\t{p} {q} {y}\n"
+      f"Закрытый ключ:\n\t{x}\n\n")
 
 while True:
     task = input('1 - Шифрование\n2 - Расшифрование\n--->')
@@ -15,16 +22,11 @@ while True:
             k = random.randint(2, p - 2)
             return pow(q, k, p), pow((pow(y, k, p) * M), 1, p)
 
-        p = sympy.randprime(2 ** 1021, 2 ** 1022)
-        q = random.randint(2, p)
-        while math.gcd(q, p) != 1:
-            q = random.randint(2, p)
-        x = random.randint(2, p - 2)
-        y = pow(q, x, p)
-        print(f"p = {p}\nq = {q}\nx = {x}\ny = {y}\n"
-              f"Открытый ключ:\n\t({p}, {q}, {y})\n"
-              f"Закрытый ключ:\n\t({x})\n\n"
-              f"\t\tШифрование:\nСообщение:\n\t{text} = {' '.join([str(i) for i in mass])}")
+        open_key = list(map(int, input('Введите Открытый ключ ->').split()))
+        p, q, y = open_key[0], open_key[1], open_key[2]
+        text = input("Введите текст для шифрования ->")
+        mass = [alfavit.find(i) for i in text]
+        print(f"\t\tШифрование:\nСообщение:\n\t{text} = {' '.join([str(i) for i in mass])}")
 
         mass_encod = []
         for i in range(len(mass)):
@@ -35,8 +37,9 @@ while True:
     elif task == '2':
         def Decoding(a, b, x, p):
             return pow((b * pow(a, (p - 1 - x), p)), 1, p)
-        p = int(input("p = "))
-        x = int(input("x = "))
+
+        x = int(input("Введите Закрытый ключ ->"))
+        p = int(input("p -> "))
         mass_encod = list(map(int, input("Введите зашифрованную последовательность ->").split(' ')))
 
         mass_decod = [Decoding(mass_encod[i * 2], mass_encod[i * 2 + 1], x, p) for i in range(len(mass_encod) // 2)]
