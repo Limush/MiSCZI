424b9f8bc7e58c038b699a8a3cfae4c0fa8ffdf8
Limush <dzaikin999@gmail.com>
2025-04-11 16:28:18 +0300

    Full

--- МиСКЗИ ЛР5.py
+++ МиСКЗИ ЛР5.py
@@ -1,56 +1,45 @@
-import random
-import sympy
-import math
+import random, sympy
 
 
-def Encrypt(text, e, n, encrypt, encrypt_text):
-    for i in range(len(text)):
-        encrypt_num = pow(text[i], e, n)
-        encrypt.append(encrypt_num)
-        encrypt_text.append(str(encrypt_num))
-    return encrypt, encrypt_text
+def Nod(a, b):
+    if a == 0 or b == 0:
+        return 0
+    while True:
+        a, b = b, a % b
+        if b == 0:
+            return a
 
 
-def Decrypt(encrypt, d, n, decrypt, decrypt_text):
-    for i in range(len(encrypt)):
-        decrypt_num = pow(encrypt[i], d, n)
-        decrypt.append(decrypt_num)
-        decrypt_text.append(str(decrypt_num))
-    return decrypt, decrypt_text
+def generat_n():
+    p, q = sympy.randprime(2 ** 1022, 2 ** 1023), sympy.randprime(2 ** 1022, 2 ** 1023)
+    return p, q, p * q
 
 
 alfavit = ' АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'
-p = sympy.randprime(2 ** 1022, 2 ** 1023)
-q = sympy.randprime(2 ** 1022, 2 ** 1023)
-while p == q:
-    q = sympy.randprime(2 ** 1022, 2 ** 1023)
-n = p * q
-m = (p - 1) * (q - 1)
-e = random.randint(2, m)
-while math.gcd(e, m) != 1:
-    e = random.randint(2, m)
-d = pow(e, -1, m)
-print(f"p = {p}\nq = {q}\nn = {n}\ne = {e}\nd = {d}\n"
-      f"Длина p = {len(bin(p))} бит, Длина q = {len(bin(q))} бит, Длина n = {len(bin(n))} бит, Длина e = {len(bin(e))} бит, Длина d = {len(bin(d))} бит\n"
-      f"\tОткрытый ключ:\n{e} {n}\n"
-      f"\tЗакрытый ключ:\n{d} {n}\n")
+p, q, n = generat_n()
+while len(bin(n)) != 2048 or p == q:
+    p, q, n = generat_n()
+fi = (p - 1) * (q - 1)
+e_open_key = random.randint(2, fi)
+while Nod(e_open_key, fi) != 1:
+    e_open_key = random.randint(2, fi)
+d_close_key = pow(e_open_key, -1, fi)
+print(f"p ({len(bin(p))} бит) = {p}\nq ({len(bin(q))} бит)  = {q}\nn ({len(bin(n))} бит)  = {n}\ne ({len(bin(e_open_key))} бит)  = {e_open_key}\nd ({len(bin(d_close_key))} бит)  = {d_close_key}")
+print(f"\tОткрытый ключ:\n{e_open_key} {n}\n\tЗакрытый ключ:\n{d_close_key} {n}\n")
 
 while True:
     task = input('1 - Шифрование\n2 - Расшифрование\n--->')
     if task == '1':
-        open_key = list(map(int, input('Введите Открытый ключ ->').split()))
-        Text = input("Введите текст для шифрования -> ")
+        open_key, Text = list(map(int, input('Введите Открытый ключ ->').split())), input("Введите текст для шифрования -> ")
         text = [alfavit.find(i) for i in Text]
-
-        encrypt, encrypt_text = Encrypt(text, open_key[0], open_key[1], [], [])
+        encrypt = [pow(text[i], open_key[0], open_key[1]) for i in range(len(text))]
         print(f"\tЗашифруем фразу:\n{Text}\n"
               f"\tФраза в цифрах:\n{' '.join([str(i) for i in text])}\n"
-              f"\tЗашифрованная последовательность:\n{' '.join(encrypt_text)}\n")
+              f"\tЗашифрованная последовательность:\n{' '.join([str(i) for i in encrypt])}\n")
     elif task == '2':
-        close_key = list(map(int, input('Введите Закрытый ключ ->').split()))
-        encrypt = list(map(int, input("Введите зашифрованную последовательность ->").split()))
-        decrypt, decrypt_text = Decrypt(encrypt, close_key[0], close_key[1], [], [])
-        print(f"\tРасшифрованная последовательность в цифрах:\n{' '.join(decrypt_text)}\n"
+        close_key, encrypt = list(map(int, input('Введите Закрытый ключ ->').split())), list(map(int, input("Введите зашифрованную последовательность ->").split()))
+        decrypt = [pow(encrypt[i], close_key[0], close_key[1]) for i in range(len(encrypt))]
+        print(f"\tРасшифрованная последовательность в цифрах:\n{' '.join([str(i) for i in decrypt])}\n"
               f"\tРасшифрованная последовательность:\n{''.join([alfavit[decrypt[i]] for i in range(len(decrypt))])}\n")
     else:
         break
